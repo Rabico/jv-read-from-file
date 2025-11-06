9d45ef92ae7cc8841ae0e7d1cd3f2bdd5ea51494
Przemysław Ciżmiński <cizminski@interia.pl>
2025-11-06 20:37:03 +0100

    Tell me how fix this wrong tests

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,38 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+        File file = new File(fileName);
+        StringBuilder builder = new StringBuilder();
+        StringBuilder builder2 = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader((file)))) {
+            String value = reader.readLine();
+
+            while (value != null) {
+                builder.append(value).append(" ");
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Cant't read from file", e);
+        }
+
+        String[] all = builder.toString().toLowerCase().replaceAll("\\p{P}", "").split(" ");
+        for (String word : all) {
+            if (word.startsWith("w")) {
+                builder2.append(word).append(" ");
+            }
+        }
+        String[] result = builder2.toString().split(" ");
+        Arrays.sort(result);
+        return result;
+
     }
 }
